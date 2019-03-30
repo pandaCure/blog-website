7d92089cd68ee98300c8cb3becf0f201c6761566
pandaCure <zzyamor@163.com>
2019-03-30 21:03:07 +0800

    测试jenkis

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React, { memo } from 'react'
+import { Provider } from 'mobx-react'
+import { Router, Switch } from 'react-router-dom'
+function Home () {
+  return (
+    <Provider>
+      <Router>
+        <div>
+          <Switch>
+            <div>1111</div>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  )
 }
 
-export default App;
+const App = memo(Home)
+
+export default App
